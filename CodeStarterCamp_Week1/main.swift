8ac518dfb6472e9ebc7a3a29a7a15083ce716791
Crois0509 <crois0509@naver.com>
2024-07-08 18:06:31 +0900

    [Round 1/Step 3] - Crois_R03

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,46 +1,21 @@
-//func printPocky(pockyBody: Int, pockyStick: Int, pockyBodyType: String, pockyTopping: Character) -> Void {
-//    for _ in 1...pockyBody {
-//        print("\(pockyTopping)\(pockyBodyType)\(pockyTopping)")
-//    }
-//    for _ in 1...pockyStick {
-//        print(" | |")
-//    }
-//}
-//var pockyBody: Int = 6
-//var pockyStick: Int = 4
-//var pockyBodyType: String = "|0|"
-//var pockyTopping: Character = " "
-//// 출력할 포키의 정보 입력
-//
-//print("""
-//<정보>
-//길이: \(pockyBody)
-//몸통: \(pockyBodyType)
-//토핑: \(pockyTopping)
-//막대길이: \(pockyStick)
-//
-//""")
-//
-//printPocky(pockyBody: pockyBody, pockyStick: pockyStick, pockyBodyType: pockyBodyType, pockyTopping: pockyTopping)
-// 이전 빼빼로 출력 스텝
+func printPepero(peperoBodyHeight: Int, peperoStickHeight: Int, peperoTopping: Character, peperoBodyType: String) {
+    print(
+        """
+        <정보>
+        길이: \(peperoBodyHeight)
+        몸통: \(peperoBodyType)
+        토핑: \(peperoTopping)
+        막대길이: \(peperoStickHeight)
 
-// 새로운 빼빼로 출력 스텝
-
-
-func printPepero(peperoHeight: Int, peperoStickHeight: Int, peperoTopping: Character, peperoBodyType: String) {
-    print("""
-<정보>
-길이: \(peperoHeight)
-몸통: \(peperoBodyType)
-토핑: \(peperoTopping)
-막대길이: \(peperoStickHeight)
-
-""")
-    for _ in 1...peperoHeight {
+        """
+    )
+    
+    for _ in 1...peperoBodyHeight {
         print("\(peperoTopping)\(peperoBodyType)\(peperoTopping)")
     }
+    
     for _ in 1...peperoStickHeight {
         print(" | |")
     }
 }
-printPepero(peperoHeight: 6, peperoStickHeight: 4, peperoTopping: " ", peperoBodyType: "|0|")
+printPepero(peperoBodyHeight: 6, peperoStickHeight: 4, peperoTopping: " ", peperoBodyType: "|0|")
