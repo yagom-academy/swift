0902b3840f6efad06a3f7328244e7084696164c3
Goohyun3436 <3436hyun@gmail.com>
2024-08-07 23:41:44 +0900

    add: Step 2 아이스크림 그리기 순서도, 코드 추가

---
+++ CodeStarterCamp_Week1/step2/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by 구현 on 8/7/24.
+//
+
+import Foundation
+
+func printElement(element: String, repeat_: Int) -> Void {
+    for _ in 1...repeat_ {
+        print(element)
+    }
+}
+
+printElement(element: "***********", repeat_: 8)
+printElement(element: "    | |", repeat_: 4)
