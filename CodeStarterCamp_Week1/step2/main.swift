9379b65f0d6a14f98f0925839ed2847b70a425db
Goohyun3436 <3436hyun@gmail.com>
2024-08-10 15:24:33 +0900

    refactor: 아이스크림 만드는 함수 2개로 분리

--- CodeStarterCamp_Week1/step2/main.swift
+++ CodeStarterCamp_Week1/step2/main.swift
@@ -7,14 +7,20 @@
 
 import Foundation
 
-func printElement(element: String, repeat_: Int) -> Void {
-    for _ in 1...repeat_ {
+func makeIceCream(width: Int, height: Int) {
+    let element = String(repeating: "*", count: width)
+    for _ in 1...height {
         print(element)
     }
 }
 
-let iceCreamSize: (Int, Int) = (11, 8)
-let stickSize: (Int, Int) = (3, 4)
+func makeStick(height: Int) {
+    let element = "    | |"
+    for _ in 1...height {
+        print(element)
+    }
+}
 
-printElement(element: String(repeating: "*", count: iceCreamSize.0), repeat_: iceCreamSize.1)
-printElement(element: String(repeating: " ", count: (iceCreamSize.0 - stickSize.0) / 2) + "| |", repeat_: stickSize.1)
+
+makeIceCream(width: 11, height: 8)
+makeStick(height: 4)
