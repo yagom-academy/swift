a974510bcde6a16ee7944e8a3349594273c8c341
Elin <byrr113@gmail.com>
2024-08-12 22:22:46 +0900

    함수 구조 변경 및 변수 선언

--- CodeStarterCamp_Week1/step3.swift
+++ CodeStarterCamp_Week1/step3.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+var lengthBody = 0, lengthStick = 0
+var body = "", topping = ""
+
+func drawStick(lengthStick: Int) {
+    for _ in 1...lengthStick {
+        print(" | |")
+    }
+}
+    
+func drawBodyPart(body: String, topping: String) {
+    print("\(topping)\(body)\(topping)")
+}
+    
+func drawBody(lengthBody: Int) {
+    for _ in 1...lengthBody {
+        drawBodyPart(body:body, topping:topping)
+    }
+}
+
+func drawPepero() {
+    lengthBody = 6
+    body = "|0|"
+    topping = " "
+    lengthStick = 4
+    
+    print("""
+    <정보>
+    길이: \(lengthBody)
+    몸통: \(body)
+    토핑: \(topping)
+    막대길이: \(lengthStick)
+    """)
+    
+    drawBody(lengthBody: lengthBody)
+    drawStick(lengthStick: lengthStick)
+}
