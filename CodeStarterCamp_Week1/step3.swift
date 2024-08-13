1bbb55bea35378b29b838d7088cd162ae9718534
Elin <byrr113@gmail.com>
2024-08-13 21:30:00 +0900

    피드백 반영 수정

--- CodeStarterCamp_Week1/step3.swift
+++ CodeStarterCamp_Week1/step3.swift
@@ -7,39 +7,37 @@
 
 import Foundation
 
-var lengthBody = 0, lengthStick = 0
-var body = "", topping = ""
-
-func drawStick(lengthStick: Int) {
-    for _ in 1...lengthStick {
+func drawStick(stickLength: Int) {
+    for _ in 1...stickLength {
         print(" | |")
     }
 }
-    
+
 func drawBodyPart(body: String, topping: String) {
-    print("\(topping)\(body)\(topping)")
+        print("\(topping)\(body)\(topping)")
 }
     
-func drawBody(lengthBody: Int) {
-    for _ in 1...lengthBody {
+func drawBody(bodyLength: Int, body: String, topping: String) {
+    for _ in 1...bodyLength {
         drawBodyPart(body:body, topping:topping)
     }
 }
 
-func drawPepero() {
-    lengthBody = 6
-    body = "|0|"
-    topping = " "
-    lengthStick = 4
-    
+func writeInfo(bodyLength: Int, body: String, topping: String, stickLength: Int) {
     print("""
     <정보>
-    길이: \(lengthBody)
+    길이: \(bodyLength)
     몸통: \(body)
     토핑: \(topping)
-    막대길이: \(lengthStick)
-    """)
+    막대길이: \(stickLength)
     
-    drawBody(lengthBody: lengthBody)
-    drawStick(lengthStick: lengthStick)
+    """)
+}
+
+func drawPepero(bodyLength: Int, body: String, topping: String, stickLength: Int) {
+    writeInfo(bodyLength: bodyLength, body: body, topping: topping, stickLength: stickLength)
+    
+    drawBody(bodyLength: bodyLength, body: body, topping: topping)
+    
+    drawStick(stickLength: stickLength)
 }
