70746601dc730ae826065e32bb4cfbd8f14c6d0a
hansol <j.hansol614@google.com>
2024-08-13 23:36:46 +0900

    add DrawPepero.swift

--- CodeStarterCamp_Week1/DrawPepero.swift
+++ CodeStarterCamp_Week1/DrawPepero.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+// 막대 그리기
+func drawPeperoStick(length: Int) {
+    for _ in 0..<length {
+        print(" | |")
+    }
+}
+
+// 토핑바디 그리기
+func drawToppingWithBody(body: String, topping: String) {
+    print("\(topping)\(body)\(topping)")
+}
+
+// 빼빼로 바디 그리기
+func drawPeperoBody(length: Int, body: String, topping: String) {
+    for _ in 0..<length {
+        drawToppingWithBody(body: body, topping: topping)
+    }
+}
+
+// 빼빼로 그리기
+func drawPepero(bodyLength: Int, body: String, topping: String, stickLength: Int) {
+    let finalTopping = topping.isEmpty ? " " : topping
+    
+    drawPeperoBody(length: bodyLength, body: body, topping: finalTopping)
+    drawPeperoStick(length: stickLength)
+}
