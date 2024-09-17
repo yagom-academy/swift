de3dc87e0eac7817bc611e05023b114ddd2138c7
jeseoyoung <jeseoyoung@seoyoung-MacBook-Pro.local>
2024-09-18 08:41:56 +0900

    style: 개행 추가,  모든 예시 출력, 개인 예시 추가

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,8 @@
 
 import Foundation
 
-drawIceCreamBody()
-drawIceCreamBar()
+drawPepero(stickLength: 4, bodyDecoration: "***", topping: "", bodyLength: 10)
+drawPepero(stickLength: 4, bodyDecoration: "***", topping: "&", bodyLength: 12)
+drawPepero(stickLength: 6, bodyDecoration: "|0|", topping: "#", bodyLength: 12)
+drawPepero(stickLength: 4, bodyDecoration: "|0|", topping: "", bodyLength: 6)
+drawPepero(stickLength: 4, bodyDecoration: "@@@", topping: "#", bodyLength: 10)
